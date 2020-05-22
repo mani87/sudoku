658a2cde3404d503246f33acd191314b5e2daa12
= <=>
2020-05-22 09:48:49 +0530

    Initial commit - Desgin app

---
+++ settings.py
@@ -0,0 +1,16 @@
+WIDTH = 600
+HEIGHT = 600
+
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHTBLUE = (96, 216, 232)
+
+# test boards
+testBoard = [[0 for x in range(9)] for x in range(9)]
+
+# Position and sizes
+gridPos = (75, 100)
+cellSize = 50
+gridSize = cellSize*9
